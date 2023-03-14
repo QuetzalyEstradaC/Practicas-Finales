9fa87e8ae9c0a592ccad700fcb25f42a136d40a5
Zaly - Quetzaly <107092686+QuetzalyEstradaC@users.noreply.github.com>
2023-03-14 13:24:02 -0600

    j
    practicas navbar 14-03-2323

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practica 1 | incluides con PHP</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <script src="js/jquery-3.6.4.min.js+"></script>
+</head>
+<body>
+    <!--navbar-->
+    <?php include "include/navbar.php";?>
+    <!--terminanavbar-->
+    <!--offcanvas -->
+    <?php include "include/menu.php"; ?>
+    <!--  terminaoffcanvas -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
