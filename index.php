972362520954bbdf4bcad12d9d79b98294df262e
Zaly - Quetzaly <107092686+QuetzalyEstradaC@users.noreply.github.com>
2023-03-16 13:33:35 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+if isset ($_POST['btn']);{
+    $Numero1=$_POST['caja1'];
+    $Numero2=$_POST['caja2'];
+    $Numero1=$_porseInt['numero1'];
+    $Numero2=$_porseInt['numero2'];
+    $Resultado=$Numero1+$Numero2;
+    $Valor=$Resultado;
+    echo $valor;
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +22,8 @@
     <script src="js/jquery-3.6.4.min.js+"></script>
 </head>
 <body>
+
+
     <!--navbar-->
     <?php include "include/navbar.php";?>
     <!--terminanavbar-->
@@ -17,5 +32,19 @@
     <!--  terminaoffcanvas -->
     <script src="js/bootstrap.min.js"></script>
 
+    <div class="col-lg-12">
+    <div class="row">
+    <h1 class="text-center">Suma</h1>
+    </div>
+
+    <div class="row">
+    <form action="'<?php echo $server [PHP_SELF];?>" method="Post";>
+    <input type="text" name="caja1" required>
+    <input type="text" name="caja2" required>
+    <input type="text" name="Resultado" required>
+    <input type="submit" name="btn" value="calcular" >;
+
+</form>
+</div>
 </body>
 </html>
